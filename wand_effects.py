f24d04a31c8e671d2f51a09c9313c6482b783b02
electriwizard <e1ectricwiz4rd@gmail.com>
2018-02-03 16:05:07 +0300

    wand is opens image succesfully

--- wand_effects.py
+++ wand_effects.py
@@ -1,20 +1,37 @@
+# -*- coding: utf-8 -*-
+# C:/Python27/python.exe
 import moviepy.editor as MpEditor
-import wand
-from wand.image import Image
-
+import wand as wnd
+import wand.image as WndImage
+import StringIO
+import png
+import scipy.misc
+from PIL import Image
+import io
+# from __future__ import print_function
 
 # def make_frame(t):
 # 	'''returns an imge of the frame at t'''
 # 	return frameFortimeT # Numpy array
 
-with Image(filename='D:\\Pictures\\Textures\\glitch.jpg') as img:
-    print('width =', img.width)
-    print('height =', img.height)
+def wand_opener(img):
+	with WndImage.Image(blob=img) as img:
+	    print('width =', img.width)
+	    print('height =', img.height) 
 
-def show_me_type(img):
-	print(type(img))
-	return img
+# Надо заставить его сожрать изображение из 
+
+def show_me_type(imarray):
+	blob = io.BytesIO()
+	inpImage = Image.fromarray(imarray, 'RGB')
+	print(inpImage)
+	inpImage.save(blob, format='JPEG')
+	# png.from_array(img)
+	print(blob)
+	# print("LOL", bytemage)
+	wand_opener(blob.getvalue())
+	return imarray
 
 # animation = MpEditor.VideoClip(make_frame, duration=3)
-x = MpEditor.VideoFileClip("D:\\Videos\\grossbeat\\AErender\\bunnies2.avi")
+x = MpEditor.VideoFileClip("D:\\Videos\\Bandicam\\bandicam 2018-01-21 21-30-59-894.mp4")
 x.fl_image(show_me_type)
